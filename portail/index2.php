247d1ace7577f6885c148dc1f01ac6f9e84aa7c5
delasalle-sio-boutin <delasalle.sio.boutin.k@gmail.com>
2016-06-10 16:29:59 +0200

    Liste des tâches + portail

---
+++ portail/index2.php
@@ -0,0 +1,70 @@
+	<?php
+// Projet DLS - BTS Info - Anciens élèves
+
+// Inclut les données du bloc <head> dans les pages HTML5 :
+//  - jeu de caractères
+//  - balises <meta>
+//  - l'icone de l'application
+//  - la feuille de styles
+
+// Modifié le 30/12/2015 par Jim
+?>
+<!doctype html>
+<html>
+<head>	
+	<?php
+// Projet DLS - BTS Info - Anciens élèves
+
+// Inclut les données du bloc <head> dans les pages HTML5 :
+//  - jeu de caractères
+//  - balises <meta>
+//  - l'icone de l'application
+//  - la feuille de styles
+
+// Modifié le 30/12/2015 par Jim
+?>
+		<title>BTS SIO - Lycée De La Salle - Rennes</title> 
+		<meta charset="utf-8">
+		<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+		<meta http-equiv=Content-Style-Type content=text/css>
+		<link rel="stylesheet" href="style2.css" type="text/css">
+
+		<!-- favicon
+		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+		<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+		<link rel="icon" type="image/png" href="images/favicon.png" />
+		-->
+</head>
+<body>
+	<div id="page">
+	
+		<div id="header">
+			<div id="header-menu">
+			</div>
+			<div id="header-logos">
+				<img src="images/Logo_DLS.gif" id="logo-gauche" alt="Lycée De La Salle (Rennes)" />
+				<img src="images/Intitules_bts_ig_sio.png" id="logo-droite" alt="BTS Informatique" />
+			</div>
+		</div>
+			
+		<div id="content">
+			 		
+			<h2>Ressources</h2>
+			
+			<div id="pages">
+			
+				<p><a href="http://sio.lyceedelasalle.fr/ae/" class="bouton-menu">Annuaire des anciens élèves</a></p>
+				<p><a href="http://sio.lyceedelasalle.fr/portefeuille/index.php" class="bouton-menu">Portefeuille de compétences</a></p>
+				<p><a href="http://sio.lyceedelasalle.fr/portail/index.php" class="bouton-menu">Documentation pour les stages</a></p>
+				
+			</div>
+			
+		</div>
+			
+		<div id="footer">
+			<p>BTS Informatique - Lycée De La Salle (Rennes)</p>
+		</div>
+	</div>
+</body>
+</html>
